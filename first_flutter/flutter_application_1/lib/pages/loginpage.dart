824173ae5e2e029bba7f6e1be6f976bf35972841
noormaniyar <nooruddin2727@gmail.com>
2021-05-31 12:04:01 +0530

    updated image for login

--- first_flutter/flutter_application_1/lib/pages/loginpage.dart
+++ first_flutter/flutter_application_1/lib/pages/loginpage.dart
@@ -36,7 +36,7 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               children: [
                 Image.asset(
-                  "assets/images/hey.png",
+                  "assets/images/login_image.png",
                   fit: BoxFit.cover,
                 ),
                 SizedBox(
